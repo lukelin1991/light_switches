e28ce09967b504d38cba7fdbd50a842872c123de
Khris Punzalan <khrispunzalan@gmail.com>
2020-07-25 15:26:55 -0500

    added state to KitchenCon

--- src/containers/KitchenCon.jsx
+++ src/containers/KitchenCon.jsx
@@ -1,18 +1,26 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export class KitchenCon extends Component {
+  state = {
+    fridge: '🎛',
+    stove: '🔥',
+    microwave: '🎚',
+  };
 
-    state = {
-        
-    }
-
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <p>Are you sure that all the things in the kitchen are off?</p>
+        <p>Please check the fridge. It is currently: {this.state.fridge}.</p>
+        <p>How about the stove? It is {this.state.stove}.</p>
+        <p>
+          Did you look at the microwave? Look again. It is{' '}
+          {this.state.microwave}.
+        </p>
+        <p>Double check everything and make sure all these things are OFF.</p>
+      </div>
+    );
+  }
 }
 
-export default KitchenCon
+export default KitchenCon;
